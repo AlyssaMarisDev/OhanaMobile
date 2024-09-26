acaebc773dacf742252e9505967cf72d8c7f9f17
Alyssa Maris <alyssamarisdev@gmail.com>
2024-09-26 10:17:50 -0700

    Feat: Add initial project for maui mobile app

---
+++ OhanaMobile/OhanaMobile/App.xaml.cs
@@ -0,0 +1,12 @@
+﻿namespace OhanaMobile
+{
+    public partial class App : Application
+    {
+        public App()
+        {
+            InitializeComponent();
+
+            MainPage = new AppShell();
+        }
+    }
+}
